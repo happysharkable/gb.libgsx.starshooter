e2040510a453fd931352f36b7ab4e3fc8db27455
Alexander Bondarenko <happy-shark@ya.ru>
2020-03-23 13:56:15 +0300

    Homework01

--- core/src/gb/libgsx/starshooter/StarGame.java
+++ core/src/gb/libgsx/starshooter/StarGame.java
@@ -10,13 +10,16 @@ import com.badlogic.gdx.graphics.g2d.TextureRegion;
 public class StarGame extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
+	Texture background;
 	TextureRegion region;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
-		region = new TextureRegion(img, 10, 10, 150, 150);
+		background = new Texture("space_bcgrnd.gif");
+		background.setWrap(Texture.TextureWrap.Repeat, Texture.TextureWrap.Repeat);
+		//region = new TextureRegion(img, 10, 10, 150, 150);
 	}
 
 	@Override
@@ -24,9 +27,10 @@ public class StarGame extends ApplicationAdapter {
 		Gdx.gl.glClearColor(0.5f, 0.7f, 0.8f, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.setColor(0.1f, 0.3f, 0.6f, 0.5f);
-		batch.draw(img, 0, 0);
-		batch.draw(region, 200, 200);
+		//batch.setColor(0.1f, 0.3f, 0.6f, 0.5f);
+		batch.draw(background, 0, 0, background.getHeight(), background.getWidth(), Gdx.graphics.getHeight(), Gdx.graphics.getWidth());
+		//batch.draw(img, 0, 0);
+		//batch.draw(region, 200, 200);
 		batch.end();
 	}
 	
