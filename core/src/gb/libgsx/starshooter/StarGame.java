812d629d071f8b955b6f2e1bc1319b01200e2714
Alexander Bondarenko <happy-shark@ya.ru>
2020-03-25 12:19:00 +0300

    added base and screen

--- core/src/gb/libgsx/starshooter/StarGame.java
+++ core/src/gb/libgsx/starshooter/StarGame.java
@@ -1,38 +1,13 @@
 package gb.libgsx.starshooter;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.Game;
 
-public class StarGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	TextureRegion region;
-	
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-		region = new TextureRegion(img, 10, 10, 150, 150);
-	}
+import gb.libgsx.starshooter.screen.MenuScreen;
+
+public class StarGame extends Game {
 
 	@Override
-	public void render () {
-		Gdx.gl.glClearColor(0.5f, 0.7f, 0.8f, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.setColor(0.1f, 0.3f, 0.6f, 0.5f);
-		batch.draw(img, 0, 0);
-		batch.draw(region, 200, 200);
-		batch.end();
-	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
+	public void create() {
+		setScreen(new MenuScreen());
 	}
 }
